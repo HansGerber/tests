b8860ac00bcad60db20ca50aa7749a69c2a9c7f2
HansGerber <hans.gerber7@mail.com>
2016-02-19 00:17:49 +0100

    first jse code

---
+++ jse.js
@@ -0,0 +1,54 @@
+/*
+ *
+ *	JSE v0.1
+ *
+ *	#
+ *	# direct onpage html editor
+ *	#
+ *
+ *	(c) 2016 by Benjamin Kaufmann
+ *
+ */
+
+(function(d, id){
+
+	var chElems = document.getElementById(id).getElementsByTagName("*");
+	var chElemLen = chElems.length;
+	for(var i = 0;i<chElemLen;i++){
+		chElemLen = chElems.length;
+		if(!chElems[i].className.match(/^jse-edit-/)){
+			var editPannel = d.createElement("div");
+			editPannel.setAttribute("class", "jse-edit-panel");
+			var ePTitle = chElems[i].tagName;
+			if(chElems[i].className && chElems[i].className.replace(" ", "").length > 0){
+				ePTitle += "." + chElems[i].className;
+			}
+			if(chElems[i].id && chElems[i].id.replace(" ", "").length > 0){
+				ePTitle += "#" + chElems[i].id;
+			}
+			editPannel.setAttribute("title", ePTitle);
+			var editButton = d.createElement("button");
+			editButton.className = "jse-edit-button";
+			editButton.innerHTML = "edit";
+			editButton.onclick = function(event) {
+				if(event){
+					event.preventDefault();
+				}
+				var _elem = this.parentNode.parentNode;
+				if(_elem.className.match(/jse-edit-activeElem/)){
+					_elem.className = _elem.className.replace("jse-edit-activeElem", "");
+				} else {
+					_elem.className += "jse-edit-activeElem";
+				}
+			}
+			editPannel.appendChild(editButton);
+			
+			if(chElems[i].getElementsByTagName("*").length > 0){
+				chElems[i].insertBefore(editPannel, chElems[i].firstChild);
+			} else {
+				chElems[i].appendChild(editPannel);
+			}
+		}
+	}
+
+})(document, "jse-container");
